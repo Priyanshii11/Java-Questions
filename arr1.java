0344057166e954338eafeb4e76c9ee9da4aacfa5
Priyanshii <priyanshik222@gmail.com>
2022-11-23 23:11:51 +0530

    second commit

---
+++ arr1.java
@@ -0,0 +1,20 @@
+//sum of all array element
+public class arr1
+{
+    public static void main(String[] args)
+    {
+        float sum=0;
+        int i;
+        float [] a=new float[5];
+        a[0] = 70.7f;
+        a[1] = 98.5f ;
+        a[2] = 98.57f ;
+        a[3] = 98.45f;
+        a[4] = 68.54f;
+        for(i=0;i<5;i++)
+        {
+            sum=sum+a[i];
+        }
+        System.out.println("sum of element"+sum);
+    }
+}
---
+++ arr2.java
@@ -0,0 +1,32 @@
+//wap to find out whether a given integer is present in array or not
+public class arr2 {
+    public static void main(String[] args) 
+    {   int i,a1,flag=0;
+        int [] a={ 1,4,5,7,9,0 };
+        a1=5;
+        for(i=0;i<=a.length;i++){
+        if(a[i]==a1)
+        {
+            flag=1;
+            break;
+        }
+        else 
+        {
+            flag=0;
+            
+        }
+    }
+        if (flag==1){
+        System.out.println("element found at "+i);
+        }
+        else
+        {
+            System.out.println("not found");
+        } 
+
+    
+    
+
+    }
+    
+}
