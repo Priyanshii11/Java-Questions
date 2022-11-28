efe802c84e230d33d55ce300fa6a097b4d02e5a8
Priyanshii <Priyanshik222@gmail.com>
2022-11-28 22:32:27 +0530

    forth commit

---
+++ nsum.java
@@ -0,0 +1,20 @@
+//wap tp sum all the natural number
+
+import java.util.Scanner;
+
+public class nsum {
+    public static void main(String[] args) {
+        System.out.println("enter the number");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int sum=0,i;
+        for (i=0;i<=a;i++)
+        {
+          sum +=i;
+          System.out.println(sum);
+            
+        }
+        System.out.println("sum of "+a+ "number is=" +sum);
+    }
+    
+}
---
+++ pattern3.java
@@ -0,0 +1,12 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=2*n-1;i++){
+           int a=(i>n)?2*n-i:i;
+           for(int j=1;j<=a;j++){
+               System.out.print("*");
+           }
+        System.out.println();
+}
+    }
+}
