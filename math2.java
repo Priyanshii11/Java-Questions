db6540f27bc947c2391b9a44a139f3a09d90617a
Priyanshii <Priyanshik222@gmail.com>
2023-01-08 20:15:09 +0530

    eleventh commit

---
+++ math2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class math2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("printing Square and Cube of number = " +a);
+        System.out.println("Sqaure of number = " +Math.pow(a, 2) +"and cube = " +Math.pow(a, 3));
+    }
+    
+}
