426831e8e0fb6032c83d2bc2315a165bde0b7881
Priyanshii <Priyanshik222@gmail.com>
2023-01-31 22:06:45 +0530

    twenty two commit

--- str2.java
+++ str2.java
@@ -3,5 +3,6 @@ public class str2 {
     public static void main(String[] args) {
          String sen="hello i m priyanshii i am a bca student";
          System.out.println(sen.replace(' ','_'));
-          }    
+       
+        }    
 }
