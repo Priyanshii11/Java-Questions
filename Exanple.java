5679f35db0d262670c84226743534106a9f71a89
Priyanshii <Priyanshik222@gmail.com>
2023-04-06 21:35:19 +0530

    29 commit

---
+++ Exanple.java
@@ -0,0 +1,18 @@
+//Constructor calling
+public class Exanple 
+{  
+
+    int a=4,b=8;
+    void display()
+    {
+        System.out.println((a+b));
+  
+    }
+
+    
+        public static void main(String[] args) 
+        {
+            Exanple cpp= new Exanple(); // creating object
+            cpp.display();//display object
+        }
+    }
